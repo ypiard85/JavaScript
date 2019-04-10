315bc86bfbe1b8e257fc91c21d6b183911c4517a
Hugo Virot <hugo.virot@gmail.com>
2019-04-10 13:48:15 +0200

    exercice 1 terminé

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,5 @@
-// you can write js here
-console.log('hello from file');
+var kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
+parseInt(kelvin);
+let celsius = kelvin-273;               //on soustrait 273 à kelvin pour obtenir celsius
+let fahrenheit = celsius * (9/5) + 32;  //on multiplie celsius par 9/5 et on lui ajoute 32 pour obtenir fahrenheit
+console.log(Math.floor(fahrenheit));    //on applique la méthode floor à farenheit pour obtenir un entier
