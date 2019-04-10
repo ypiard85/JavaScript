645f9b13a66b245fc27e21bfd6272caabd4c1244
Hugo Virot <hugo.virot@gmail.com>
2019-04-10 14:37:43 +0200

    exercice 2 terminé

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,5 +1,5 @@
 var kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
-parseInt(kelvin);
+parseInt(kelvin);                       //on convertit la saisie en nombre entier
 let celsius = kelvin-273;               //on soustrait 273 à kelvin pour obtenir celsius
 let fahrenheit = celsius * (9/5) + 32;  //on multiplie celsius par 9/5 et on lui ajoute 32 pour obtenir fahrenheit
 console.log(Math.floor(fahrenheit));    //on applique la méthode floor à farenheit pour obtenir un entier
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,32 @@
 // you can write js here
 console.log('exo-2');
+let inscriptionCoureur = false;    //cette variable est un booléen. Test avec false
+let ageCoureur = 99;               //test avec 99
+var raceNumber = Math.floor(Math.random()*1000);
+
+if (inscriptionCoureur != true)
+{
+    var raceNumber = raceNumber+1000;
+}
+
+if ((ageCoureur<18) && (inscriptionCoureur === true))
+{
+    console.log('You will race at 9:30 am');
+}
+
+else if ((inscriptionCoureur === true) || (ageCoureur>18))
+{
+    console.log ('you will race at 11:00 am');
+}
+
+else if ((ageCoureur<18) || (inscriptionCoureur === false))
+{
+    console.log ('you will race at 12:30 am');
+    console.log (raceNumber);
+}
+
+else 
+{
+    console.log ('Go to see the registration desk');
+}
+console.log (raceNumber);
